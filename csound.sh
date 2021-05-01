e5560ba69b9dbac38e65cd09f4f95503f96f5018
remku <83505774+remku@users.noreply.github.com>
2021-05-01 20:22:12 +0530

    Create csound.sh

---
+++ csound.sh
@@ -0,0 +1,19 @@
+red=\e[1;91m
+green=\e[1;92m
+white=\e[0m
+loc=/system/product/media/audio/ui
+su -v
+if [ "$?" -ne "0" ]; then
+  echo -e "${red}*** No root permission ***${white}"
+  exit
+fi
+if [ -e "$loc/camera_click.ogg" ]; then
+  su -c mv $loc/camera_click.ogg $loc/cam
+  echo -e "Camera & Screenshot sound -> ${red} OFF"
+else
+  su -c mv $loc/cam $loc/camera_click.ogg
+  echo -e "Camera & Screenshot sound -> ${green} ON"
+fi
+echo "-------------------------"
+ls --color $loc | grep cam
+echo "-------------------------"
