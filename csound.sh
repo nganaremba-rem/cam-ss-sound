ccb35be5b6b0fd4f73fb473e369aa215960dbf9f
remku <83505774+remku@users.noreply.github.com>
2021-05-01 20:27:49 +0530

    Update csound.sh

--- csound.sh
+++ csound.sh
@@ -8,14 +8,16 @@ if [ "$?" -ne "0" ]; then
   exit
 else
   echo -e "Root Permission-> ${green} OK ${white}"
+  su -c mount -o remount,rw /
 fi
 if [ -e "$loc/camera_click.ogg" ]; then
   su -c mv $loc/camera_click.ogg $loc/cam
-  echo -e "Camera & Screenshot sound -> ${red} OFF"
+  echo -e "Camera & Screenshot sound -> ${red} OFF ${white}"
 else
   su -c mv $loc/cam $loc/camera_click.ogg
-  echo -e "Camera & Screenshot sound -> ${green} ON"
+  echo -e "Camera & Screenshot sound -> ${green} ON ${white}"
 fi
+su -c mount -o remount,ro /
 echo "-------------------------"
 ls --color $loc | grep cam
 echo "-------------------------"
