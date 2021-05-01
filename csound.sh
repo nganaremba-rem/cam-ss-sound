eea46a03117457d945b6f317376a881c38f447b6
remku <83505774+remku@users.noreply.github.com>
2021-05-01 20:25:53 +0530

    Update csound.sh

--- csound.sh
+++ csound.sh
@@ -1,11 +1,13 @@
-red=\e[1;91m
-green=\e[1;92m
-white=\e[0m
-loc=/system/product/media/audio/ui
+red="\e[1;91m"
+green="\e[1;92m"
+white="\e[0m"
+loc="/system/product/media/audio/ui"
 su -v
 if [ "$?" -ne "0" ]; then
   echo -e "${red}*** No root permission ***${white}"
   exit
+else
+  echo -e "Root Permission-> ${green} OK ${white}"
 fi
 if [ -e "$loc/camera_click.ogg" ]; then
   su -c mv $loc/camera_click.ogg $loc/cam
